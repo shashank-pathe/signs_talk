bdd069ec7edfb25ad8ee6b9aa044c8b151a78f54
shashank <nayakaishu1999@gmail.com>
2024-04-27 23:05:56 +0530

    i am aishvarya nayak

---
+++ src/components/main.jsx
@@ -0,0 +1,38 @@
+<main>
+      <div class="flex">
+        <div class="main hero">
+          <div id="quote">
+            Share Your <span class="main-txt"> Feelings <br /> </span> By
+            <span class="main-txt">Actions</span>.
+          </div>
+          <div id="underquote">
+            Here,You can easily talk with your <br />
+            fellow's by sign language Translator.
+          </div>
+          <div class="login">
+            <button type="button" id="register">
+              Register for more benifits
+            </button>
+          </div>
+        </div>
+        <div>
+          <img id="ani" src="handstalk.gif" alt="" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="box orange"></div>
+        <div class="box voilet"></div>
+        <div class="box yellow"></div>
+        <div class="box green"></div>
+        <div class="box blue"></div>
+      </div>
+      <div class="screen">
+        <p class="heading">
+          Meanwhile, download the Sign Talk App for free and <br />start
+          learning a new language today!
+        </p>
+        <div class="mario"></div>
+        <div class="text"></div>
+      </div>
+    </main>
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+<nav class="navbar">
+      <div class="logo item">
+        <img id="logo" src="\signtalk.png" />
+      </div>
+
+      <div class="home item">Home</div>
+      <div class="about item">About Us</div>
+      <div class="contact item">Contact Us</div>
+      <div class="blog item">Blog</div>
+      <div class="dropdown">
+        <span>Mouse over me</span>
+        <div class="dropdown-content">
+        <p>Hello World!</p>
+        </div>
+      </div>
+    </nav>
